20bc37b2b655db378038a3682179161223095ca9
cheapmankrabs <estauff12@gmail.com>
2020-09-15 22:12:24 -0600

    changed name of flask folder

--- rest_api/__init__.py
+++ rest_api/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from flaskr import routes
+from rest_api import routes
--- rest_api/routes.py
+++ rest_api/routes.py
@@ -1,12 +1,10 @@
 from flask import request, jsonify
-from flaskr import app
-from flaskr import Search
+from rest_api import app
+from rest_api import Search
 
-
+@app.route('/',methods=['POST'])
 @app.route('/search', methods=['POST'])
 def search():
     search_input = request.json
-
     response = Search.main(search_input["phrase"])
-
     return response
